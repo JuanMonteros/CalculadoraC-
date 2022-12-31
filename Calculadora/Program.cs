a94eec7b788c74788abc275203937968e05fa88c
JuanMonteros <juanglassalle80@gmail.com>
2022-12-30 21:26:25 -0300

    Calculadora

---
+++ Calculadora/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Suma sum = new Suma(10, 5);
+            int res=sum.Resultado();
+            Console.WriteLine(res);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Calculadora/Suma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Suma
+    {
+        private int numeroA;
+        private int numeroB;
+
+        public int NumeroA { get => numeroA; set => numeroA = value; }
+        public int NumeroB { get => numeroB; set => numeroB = value; }
+
+        public Suma(int _numA,int _numB)
+        {
+            numeroA = _numA;
+            numeroB = _numB;
+
+        }
+        public int Resultado() => numeroA + numeroB;
+    }
+}
